87657a77a51b768356777a90a723f4d4b180ae95
Tany <arhiza@gmail.com>
2023-02-07 23:03:12 +0300

    table name settings

--- app_users/models.py
+++ app_users/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class SiteSettings(models.Model):
     key = models.CharField(max_length=10, verbose_name="Название параметра", unique=True)
     value = models.CharField(max_length=100, verbose_name="Значение параметра")
+
+    class Meta:
+        verbose_name_plural = 'Настройки сайта'
+        verbose_name = 'Настройка сайта'
+
