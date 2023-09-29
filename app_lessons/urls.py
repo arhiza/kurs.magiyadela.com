3f6ce90810a286553cdff16443f902c089f9e1eb
Tany <arhiza@gmail.com>
2023-09-29 03:37:37 +0300

    remove exp_code

--- app_lessons/urls.py
+++ app_lessons/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 
-from .views import CourseView, LessonView, CourseListView, AddCommentView, TMPView, TMPMobileView
+from .views import CourseView, LessonView, CourseListView, AddCommentView
 
 urlpatterns = [
     path('', CourseListView.as_view(), name='all_courses'),
     path('kurs/<slug:slug>', CourseView.as_view(), name='course'),
     path('urok/<slug:slug>', LessonView.as_view(), name='lesson'),
     path('comment/<slug:slug>', AddCommentView.as_view(), name='comment'),
-    #path('tmp/', TMPView.as_view(), name='for_mobile_experiments'),
-    #path('tmp_mobile/', TMPMobileView.as_view(), name='for_mobile_experiments'),
 ]
